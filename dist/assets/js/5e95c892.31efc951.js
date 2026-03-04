7b846b542e1c91d9f761b6e299901ed9dcf30215
Your Name <you@example.com>
2026-03-04 22:43:41 +0200

    style: replace default docusaurus favicon with matrix-themed logo svg

---
+++ dist/assets/js/5e95c892.31efc951.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkopenexec_docs=globalThis.webpackChunkopenexec_docs||[]).push([[647],{7121(e,s,c){c.r(s),c.d(s,{default:()=>d});c(6540);var r=c(4164),a=c(7559),o=c(5500),n=c(2831),u=c(1656),l=c(4848);function d(e){return(0,l.jsx)(o.e3,{className:(0,r.A)(a.G.wrapper.docsPages),children:(0,l.jsx)(u.A,{children:(0,n.v)(e.route.routes)})})}}}]);
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'OpenExec Docs',
   tagline: 'Orchestrate the Reality',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   url: 'https://docs.openexec.io',
   baseUrl: '/',
